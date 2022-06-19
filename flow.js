1ad3203431757e47005aef7db8e96507b69f0e6d
Jesse <jessejkilback@gmail.com>
2022-06-19 15:52:15 -0700

    Fix links Add script to create element

--- flow.js
+++ flow.js
@@ -1,5 +1,6 @@
-document.getElementById("addButton").addEventListener('click',addNozzle());
-addNozzle(){
+const addButton = document.getElementById("addButton");
+addButton.addEventListener("click", addNozzle);
+function addNozzle(){
     let nozzles = document.getElementById("nozzles");
     let nozzleType = document.createElement("select");
     nozzles.appendChild(nozzleType);
