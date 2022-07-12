79f7fef02538ab13d8e3acf9915d38f2a3629c31
Jesse <jessejkilback@gmail.com>
2022-07-11 17:08:44 -0700

    fix script to append items

--- flow.js
+++ flow.js
@@ -1,5 +1,6 @@
 const addButton = document.getElementById("addButton");
 addButton.addEventListener("click", addNozzle);
+
 function addNozzle(){
     let nozzles = document.getElementById("nozzles");
     let nozzleType = document.createElement("select");
