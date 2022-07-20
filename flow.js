801d3afe08b8a2b83d161caec6f963c6b3714109
Jesse <jessejkilback@gmail.com>
2022-07-19 17:18:05 -0700

    add selector values

--- flow.js
+++ flow.js
@@ -1,8 +1,15 @@
 const addButton = document.getElementById("addButton");
+const nozzles = document.getElementById("nozzles");
 addButton.addEventListener("click", addNozzle);
 
 function addNozzle(){
-    let nozzles = document.getElementById("nozzles");
+
     let nozzleType = document.createElement("select");
+    let rainbird = document.createElement('option');
+    rainbird.text = "Rainbird";
+    let toro = document.createElement('option');
+    toro.text = "Toro";
+    nozzleType.add(option);
+ 
     nozzles.appendChild(nozzleType);
 }
