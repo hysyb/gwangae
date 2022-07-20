e37a5c79f96fc456fb82f4a1e0e8e8049440a4e7
Jesse <jessejkilback@gmail.com>
2022-07-19 18:03:37 -0700

    fix bug

--- flow.js
+++ flow.js
@@ -2,23 +2,49 @@ const addButton = document.getElementById("addButton");
 const nozzles = document.getElementById("nozzles");
 addButton.addEventListener("click", addNozzle);
 
+
 function addNozzle(){
     let rowContainer = document.createElement('div');
     nozzles.appendChild(rowContainer);
-    let nozzleType = document.createElement("select");
-    let rainbird = document.createElement('option');
+    let nozzleBrand = document.createElement("select");
 
+    let defaultBrand = document.createElement('option');
+    defaultBrand.text = "Select A Brand";
+    nozzleBrand.add(defaultBrand);
+
+    
+    let rainbird = document.createElement('option');
     rainbird.text = "Rainbird";
+    nozzleBrand.add(rainbird);
+
     let toro = document.createElement('option')
     toro.text = "Toro";    
-    nozzleType.add(rainbird);
-    nozzleType.add(toro);
+    nozzleBrand.add(toro);
 
-    nozzleType.addEventListener('change', () => {
-        let text = nozzleType.options[nozzleType.selectedIndex].text;
-        console.log(text);
-        }
-    )
- 
-    rowContainer.appendChild(nozzleType);
+    rowContainer.appendChild(nozzleBrand);
+
+    nozzleBrand.addEventListener('change', () => {
+        let text = nozzleBrand.options[nozzleBrand.selectedIndex].text;
+       
+       // Adds too many options on double select
+        // if (text == "Rainbird") {
+      //      while (nozzleBrand.hasChildNodes()) {
+     //           nozzleBrand.removeChild(nozzleBrand.lastChild);
+      //      }
+            let nozzleType = document.createElement('select');
+            
+            let defaultType = document.createElement('option');
+             defaultType.text = "Select a Nozzle Type";
+             nozzleType.add(defaultType);
+           
+            let quarter5 = document.createElement('option');
+             quarter5.text = "5Q";
+             nozzleType.add(quarter5);
+             let half5 = document.createElement('option');
+             half5.text = "5H";
+             nozzleType.add(half5);
+             rowContainer.appendChild(nozzleType);
+
+        })
 }
+
